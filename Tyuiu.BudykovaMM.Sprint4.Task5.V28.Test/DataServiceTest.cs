ebaa752b370b1caad1e463b76ac6ff80afe1d844
Мадина <mbudykova@mail.ru>
2025-11-03 22:05:56 +0500

    Task5

---
+++ Tyuiu.BudykovaMM.Sprint4.Task5.V28.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.BudykovaMM.Sprint4.Task5.V28.Lib;
+namespace Tyuiu.BudykovaMM.Sprint4.Task5.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+
+            int[,] mas2 = new int[5, 5] { {3, -4, -5, 1, 2 },
+                                         { 0, -4, 1, 3, 2 },
+                                         { -4, 3, 2, 1, 2 },
+                                         { 3, 3, -6, 2, 1 },
+                                        { -6, 3, -5, -4, -6 } };
+            int[,] res = ds.Calculate(mas2);
+            int[,] wait = new int[5, 5] { {3, 0, 0, 1, 2 },
+                                         { 0, 0, 1, 3, 2 },
+                                         { 0, 3, 2, 1, 2 },
+                                         { 3, 3, 0, 2, 1 },
+                                        { 0, 3, 0, 0, 0 } };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BudykovaMM.Sprint4.Task5.V28/Program.cs
@@ -0,0 +1,70 @@
+﻿using Tyuiu.BudykovaMM.Sprint4.Task5.V28.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Random rnd = new Random();
+
+        Console.Title = "Спринт #4 │ Выполнила: Будыкова М. М. │ ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #28                                                              *");
+        Console.WriteLine("* Тема: Двумерные массивы (генератор случайных чисел)                     *");
+        Console.WriteLine("* Вариант #28                                                             *");
+        Console.WriteLine("* Выполнила: Будыкова Мадина Маратовна │ ПИНб-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов                     *");
+        Console.WriteLine("* заполненный случайными значениями в диапазоне от -6 до 3                *");
+        Console.WriteLine("* Заменить отрицательные элементы массива на 0                            *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.Write("Введите количество строк в массиве: ");
+        int rows = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите количество столбцов в массиве: ");
+        int columns = Convert.ToInt32(Console.ReadLine());
+
+        int[,] mtrx = new int[rows, columns];
+
+        Console.WriteLine("***************************************************************************");
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                mtrx[i, j] = rnd.Next(-6, 4);
+            }
+        }
+
+        Console.WriteLine("\nИсходный массив: ");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{mtrx[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        int[,] res = ds.Calculate(mtrx);
+        Console.WriteLine("Измененный массив: ");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{res[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+}
