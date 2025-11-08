ef8866ff8b9084c42341b812274a5ad1c4172854
Мадина <mbudykova@mail.ru>
2025-11-08 14:51:00 +0500

    Task6

---
+++ Tyuiu.BudykovaMM.Sprint4.Task6.V29.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Tyuiu.BudykovaMM.Sprint4.Task6.V29.Lib;
+namespace Tyuiu.BudykovaMM.Sprint4.Task6.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            var season = new string[] { "Весна", "Лето", "Осень", "Зима" };
+            int res = ds.Calculate(season);
+            int wait = 2;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BudykovaMM.Sprint4.Task6.V29/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.BudykovaMM.Sprint4.Task6.V29.Lib;
+internal class Program
+    // используя класс Array подсчитайте количество элементов, длина которых больше 4
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 │ Выполнила: Будыкова М. М. │ ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Класс Array                                                       *");
+        Console.WriteLine("* Вариант #29                                                             *");
+        Console.WriteLine("* Выполнила: Будыкова Мадина Маратовна │ ПИНб-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан строковый массив данных [Весна, Лето, Осень, Зима]                  *");
+        Console.WriteLine("* заполненный статическими значениями в диапазоне от 0 до 9. Используя    *");
+        Console.WriteLine("* класс Array подсчитайте количество элементов, длина которых больше 4.   *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        var season = new string[] { "Весна", "Лето", "Осень", "Зима" };
+
+        Console.WriteLine("Исходный массив: ");
+        for (int i = 0; i <= season.Length -1; i++)
+        {
+            Console.WriteLine(season[i]);
+        }
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Количество элементов, длина которых больше 4: " + ds.Calculate(season));
+        Console.ReadKey();
+    }
+}
